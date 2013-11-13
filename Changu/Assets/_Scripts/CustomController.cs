640c3402a08a7cb50eb060d51c273006c629f26d
Gabriel Violette <phoniks7@hotmail.com>
2013-11-13 02:23:41 -0500

    Added dodge functionality. +Shield functionality +Berserk defense Changed the tiles, added some regenerations.

--- Changu/Assets/_Scripts/CustomController.cs
+++ Changu/Assets/_Scripts/CustomController.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class CustomController : MonoBehaviour 
 {
 
+	#region vars
+	public int facing = 3; //direction for sprites (0: Right, 1: Up, 2: Left, 3: Down)
+
+	[HideInInspector]
+	public bool move_enabled = true; //for disabling motion
+
+	public float speed = 2.0f; //unity units per second (we're using Tile Size pixels per unit (64) )
+	#endregion
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -16,25 +25,67 @@ public class CustomController : MonoBehaviour
 		Vector2 move_vec = new Vector2( 0.0f, 0.0f );
 
 		#region input parsing
-		if ( Input.GetKey( KeyCode.A ) )
+		if ( move_enabled )
 		{
-			move_vec.x -= 1;
-		}
-		if ( Input.GetKey ( KeyCode.D ) )
-		{
-			move_vec.x += 1;
-		}
-		if ( Input.GetKey ( KeyCode.S ) )
-		{
-			move_vec.y -= 1;
-		}
-		if ( Input.GetKey ( KeyCode.W ) )
-		{
-			move_vec.y += 1;
+			if ( Input.GetKey( KeyCode.A ) )
+			{
+				move_vec.x -= 1;
+			}
+			if ( Input.GetKey ( KeyCode.D ) )
+			{
+				move_vec.x += 1;
+			}
+			if ( Input.GetKey ( KeyCode.S ) )
+			{
+				move_vec.y -= 1;
+			}
+			if ( Input.GetKey ( KeyCode.W ) )
+			{
+				move_vec.y += 1;
+			}
 		}
 		#endregion
 
-		if ( move_vec.sqrMagnitude > 0.0f )
+		#region facing parsing
+		//mouse controls dir?
+		//ASSUMPTION: player will always be centered!
+		//otherwise we need to cast x/y -> screen x y.
+
+		float angle = Mathf.Rad2Deg * Mathf.Atan2 ( Input.mousePosition.y - Screen.height / 2, Input.mousePosition.x - Screen.width / 2 );
+
+		//pull into range?
+		if ( angle < 0.0f )
+		{
+			angle += 360.0f;
+		}
+		else if ( angle > 360.0f )
+		{
+			angle -= 360.0f;
+		}
+
+		//Based on angle, get facing.
+		if ( (angle >= 0.0f && angle <= 45.0f) || (angle >= 315.0f && angle <= 360.0f) )
+		{
+			facing = 0;
+		}
+		else if ( angle >= 45.0f && angle <= 135.0f )
+		{
+			facing = 1;
+		}
+		else if ( angle >= 135.0f && angle <= 225.0f )
+		{
+			facing = 2;
+		}
+		else if ( angle >= 225.0f && angle <= 315.0f )
+		{
+			facing = 3;
+		}
+
+		//Debug.Log ( angle + " : " + facing ); //DEBUG LINE
+
+		#endregion
+
+		if ( move_vec.sqrMagnitude > 0.0f ) //sqrMagnitude is more efficient.
 		{
 			//Move.
 			//Get direction from vec.
@@ -44,11 +95,16 @@ public class CustomController : MonoBehaviour
 			float y = this.gameObject.transform.position.y;
 			float z = this.gameObject.transform.position.z;
 
-			//Scale to frame speed
-			move_vec = move_vec * Time.deltaTime;
+			//Scale to speed
+			move_vec = move_vec * speed * Time.deltaTime;
 
 			//Actually move
 			this.gameObject.transform.position = new Vector3( x + move_vec.x, y + move_vec.y, z );
+
+			//Force Camera to lock onto player
+			Camera.main.transform.position = new Vector3( this.gameObject.transform.position.x,
+			                                              this.gameObject.transform.position.y,
+			                                              Camera.main.transform.position.z );
 		}
 		else
 		{
--- Changu/Assets/_Scripts/PlayerCore.cs
+++ Changu/Assets/_Scripts/PlayerCore.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerCore : MonoBehaviour 
 {
 	#region vars
+	CustomController move_controller;
+
 	//HP
 	public float hp = 100.0f;
 	public float max_hp = 100.0f;
@@ -11,30 +13,51 @@ public class PlayerCore : MonoBehaviour
 	//Stamina
 	public float sta = 100.0f;
 	public float max_sta = 100.0f;
+	float sta_regen_rate = 10.0f; //stamina regen per sec.
 
 	int generation = 0;
 
 	string character_class; //ENUM: melee, ranged, ...
-	string relic; //ENUM: shield, boots, berserk
+	string relic = "boots"; //ENUM: shield, boots, berserk
 	
 	float berserk_t = 0.0f;
 	bool berserk_on = false;
 	float berserk_sta_cost;
+
+	float berserk_dur = 15.0f; //seconds
+	float berserk_dmg_multiplier = 2.0f; //damage amp. factor
+	float berserk_def_multiplier = 0.5f; //damage intake reduced by this percent.
+	float berserk_cooldown_t;
+	float berserk_cooldown_total = 10.0f; //seconds
 	//cast time
 	
 	bool shield_on = false;
 	float shield_sta_cost = 10.0f; //multiplied by damage?
 	float shield_dmg;      //current damage accumulation
-	float shield_max_dmg;  //shield breaker damage
+	float shield_max_dmg = 20.0f;  //shield breaker damage
+	float shield_regen_rate = -10.0f; //dmg / sec
 	//cast time
 
-	float boots_t = 0.0f;
 	bool boots_on = false;
 	float boots_sta_cost = 10.0f;
+	float boots_t = 0.0f;
+	float boots_duration = 0.5f;
+	float boots_invincible_duration = 7.5f / 60.0f; //7.5 frames of invulnerability
+	float boots_speed = 8.0f; //unity units / s
+	Vector2 boots_dir = new Vector2( 1.0f, 0.0f );
+	bool dodge_enqueued = false;
+	Vector2 dodge_dir = new Vector2( 0, 0 );
 	//cast/ani time
 
 
 	#endregion
+
+	//Use this for pre-initialization
+	void Awake()
+	{
+		//set up links
+		move_controller = this.gameObject.GetComponent<CustomController>();
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -46,6 +69,72 @@ public class PlayerCore : MonoBehaviour
 	void Update () 
 	{
 		HandleInput ();
+
+		//regens
+		sta = Mathf.Min( sta + sta_regen_rate * Time.deltaTime, max_sta );
+		shield_dmg = Mathf.Max( shield_dmg + shield_regen_rate * Time.deltaTime, 0.0f );
+		if ( berserk_cooldown_t > 0.0f )
+		{
+			berserk_cooldown_t -= Time.deltaTime;
+		}
+
+		//Berserk on/off.
+		if ( berserk_on == true )
+		{
+			berserk_t -= Time.deltaTime;
+			if ( berserk_t <= 0.0f )
+			{
+				berserk_on = false;
+				//TODO: turn off berserk effect (highlight?)
+			}
+		}
+
+		//Dodge on/off.
+		if ( boots_on == true )
+		{
+			boots_t -= Time.deltaTime;
+			if ( boots_t <= 0.0f )
+			{
+				if ( dodge_enqueued == true)
+				{
+					dodge_enqueued = false;
+					//TODO: refactor
+					if ( sta >= boots_sta_cost )
+					{
+						sta -= boots_sta_cost;
+						boots_dir = dodge_dir;
+						boots_t = boots_duration;
+					}
+					else
+					{
+						//not enough stamina
+						//do breath ani.
+					}
+				}
+				else
+				{
+					//Turn off
+					boots_on = false;
+					move_controller.move_enabled = true;
+				}
+			}
+			else
+			{
+				//MOVE!
+				float x = this.gameObject.transform.position.x;
+				float y = this.gameObject.transform.position.y;
+				float z = this.gameObject.transform.position.z;
+				float x_plus = boots_dir.x * boots_speed * Time.deltaTime;
+				float y_plus = boots_dir.y * boots_speed * Time.deltaTime;
+
+				this.gameObject.transform.position = new Vector3(x + x_plus, y + y_plus, z);
+
+				//Force Camera to lock onto player
+				Camera.main.transform.position = new Vector3( this.gameObject.transform.position.x,
+				                                             this.gameObject.transform.position.y,
+				                                             Camera.main.transform.position.z );
+			}
+		}
 	}
 
 	void HandleInput()
@@ -87,6 +176,11 @@ public class PlayerCore : MonoBehaviour
 					if ( sta >= boots_sta_cost )
 					{
 						sta -= boots_sta_cost;
+						move_controller.move_enabled = false;
+						boots_on = true;
+						boots_t = boots_duration;
+						boots_dir = GetBootDir();
+						//get dir, start moving
 						//animate
 					}
 					else
@@ -97,7 +191,10 @@ public class PlayerCore : MonoBehaviour
 				}
 				else
 				{
-					//boots are still animating
+					//boots are still animating (enqueue another dodge?)
+					dodge_enqueued = true;
+					//set direction
+					dodge_dir = GetBootDir();
 				}
 			}
 			#endregion
@@ -109,10 +206,18 @@ public class PlayerCore : MonoBehaviour
 				{
 					if ( sta >= berserk_sta_cost )
 					{
-						sta -= berserk_sta_cost;
-						berserk_on = true;
-						berserk_t = 60; //TODO: formula all over it.
-						//animate
+						if ( berserk_cooldown_t <= 0 )
+						{
+							sta -= berserk_sta_cost;
+							berserk_on = true;
+							berserk_t = berserk_dur;
+							berserk_cooldown_t = berserk_cooldown_total + berserk_dur;
+							//Pause game + special animation?
+						}
+						else
+						{
+							//Cooldown hasn't expired yet.
+						}
 					}
 					else
 					{
@@ -124,6 +229,11 @@ public class PlayerCore : MonoBehaviour
 				{
 					//toggle it off. (reduces CD)
 					berserk_on = false;
+
+					float percent_used = ( (berserk_dur - berserk_t) / berserk_dur);
+					berserk_cooldown_t -= (1.0f - percent_used) * (0.5f * berserk_cooldown_total + berserk_dur); //prorate the cooldown.
+					//50% penalty applied to the cooldown portion to keep from spamming it on/off?
+
 					berserk_t = 0;
 				}
 			}
@@ -134,9 +244,7 @@ public class PlayerCore : MonoBehaviour
 			#region shield
 			if ( shield_on == true )
 			{
-				shield_on = false;
-				//Un-do motion modifications
-				//animate
+				TurnOffShield();
 			}
 			#endregion
 		}
@@ -145,7 +253,6 @@ public class PlayerCore : MonoBehaviour
 		#region attacks
 		if ( Input.GetMouseButtonUp( 0 ) )
 		{
-			hp -= 1.0f;
 			//attacks
 			if ( character_class == "melee" )
 			{
@@ -157,5 +264,58 @@ public class PlayerCore : MonoBehaviour
 			}
 		}
 		#endregion
+	}
+
+	void TurnOffShield()
+	{
+		shield_on = false;
+		//Un-do motion modifications
+		//animate
+	}
+
+	Vector2 GetBootDir()
+	{
+		//returns the normalized direction vector to dodge towards.
+		float angle = Mathf.Atan2 ( Input.mousePosition.y - Screen.height / 2, Input.mousePosition.x - Screen.width / 2 );
+		return new Vector2( Mathf.Cos ( angle ), Mathf.Sin ( angle ) );
+	}
+
+	void OnHit( float dmg )
+	{
+		//Player was hit by something
+
+		//Mitigation:
+		if ( shield_on == true )
+		{
+			//check if it breaks the shield
+			shield_dmg += dmg;
+			if ( shield_dmg >= shield_max_dmg )
+			{
+				TurnOffShield();
+			}
+
+			//ignore damage
+			//TODO: decide block or not based on impact angle.
+			//TODO: damage % cutout? (ie getting hit does 10% damage, a break does 20%?)
+			dmg = 0.0f;
+			//animate block
+		}
+		else if ( boots_on == true && boots_t > boots_duration - boots_invincible_duration )
+		{
+			//ignore damage, dodged it with i-frames.
+			dmg = 0.0f;
+		}
+		else
+		{
+			if ( berserk_on == true )
+			{
+				//take reduced damage
+				dmg = ( 1.0f - berserk_def_multiplier) * dmg;
+			}
+
+			hp -= dmg;
+			//animate
+			//check death?
+		}
 	}
 }
