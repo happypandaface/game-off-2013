4a36239827c8e045619753646c49fc24ffca01a3
Gabriel Violette <phoniks7@hotmail.com>
2013-11-13 22:48:50 -0500

    Hooked up the dungeon generator.

---
+++ Changu/Assets/_Scripts/DungeonGenerator.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Tile
+{
+	public string type;
+	public int xPos;
+	public int yPos;
+}
+
+public class Room
+{
+	public int width;
+	public int height;
+	public int xPos;
+	public int yPos;
+	
+	public void make(int x, int y, int w, int h)
+	{
+		xPos = x;
+		yPos = y;
+		width = w;
+		height = h;
+	}
+	
+	public void addTo(ref ArrayList dungeon)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				Tile ts = new Tile();
+				ts.type = "floor";
+				ts.xPos = xPos+x;
+				ts.yPos = yPos+y;
+				dungeon.Add(ts);
+			}
+		}
+	}
+	
+	public Vector2 getRandomPos(float seed)
+	{
+		int x = (int)(Random.value*width)+xPos;
+		int y = (int)(Random.value*height)+yPos;
+		return new Vector2(x, y);
+	}
+}
+
+public class Corridor
+{
+	public Room room1;
+	public Room room2;
+	
+	public void make(Room r1, Room r2)
+	{
+		room1 = r1;
+		room2 = r2;
+	}
+	
+	public void addTo(ref ArrayList dungeon)
+	{
+		// the following code assumes the rooms aren't overlapping
+		if (room1.xPos <= room2.xPos+room2.width)
+		{
+			if (room1.xPos + room1.width >= room2.xPos)
+			{
+				if (room1.xPos <= room2.xPos+room2.width && room1.xPos >= room2.xPos)
+				{
+					
+				}else
+					if (room1.xPos+room1.width <= room2.xPos+room2.width && room1.xPos+room1.width >= room2.xPos)
+				{
+					
+				}else
+				{
+					//do center corridor
+				}
+			}
+		}
+		else
+			if (room1.yPos <= room2.yPos+room2.height)
+		{
+			if (room1.yPos + room1.height >= room2.yPos)
+			{
+				if (room1.yPos <= room2.yPos+room2.height && room1.yPos >= room2.yPos)
+				{
+					if (room1.xPos < room2.xPos)
+					{
+						
+					}else
+					{
+						for (int x = room2.xPos+room2.width; x < room1.xPos; x++)
+						{
+							Tile ts = new Tile();
+							ts.type = "floor";
+							ts.xPos = x;
+							ts.yPos = room1.yPos;
+							dungeon.Add(ts);
+						}
+					}
+				}else
+					if (room1.yPos+room1.height <= room2.yPos+room2.width && room1.yPos+room1.height >= room2.yPos)
+				{
+					
+				}else
+				{
+					//do center corridor
+				}
+			}
+		}else
+		{
+			//do hooked corridor
+		}
+	}
+}
+
+public class DungeonGenerator
+{
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+	
+	public ArrayList generateDungeon(float seed)
+	{
+		Random.seed = (int)(seed*100000.0f);
+		ArrayList dungeon = new ArrayList();
+		Room r = new Room();
+		r.make (1, 1, 5, 7);
+		r.addTo (ref dungeon);
+		Room r2 = new Room();
+		r2.make (-7, 1, 5, 6);
+		r2.addTo (ref dungeon);
+		Corridor c = new Corridor();
+		c.make (r, r2);
+		c.addTo (ref dungeon);
+		Vector2 spawnPos = r.getRandomPos(seed);
+		Tile ts = new Tile();
+		ts.type = "spawn";
+		ts.xPos = (int)spawnPos.x;
+		ts.yPos = (int)spawnPos.y;
+		dungeon.Add(ts);
+		Vector2 enPos = r2.getRandomPos(seed);
+		Tile en = new Tile();
+		en.type = "enemy";
+		en.xPos = (int)enPos.x;
+		en.yPos = (int)enPos.y;
+		dungeon.Add(en);
+		return dungeon;
+	}
+}
---
+++ Changu/Assets/_Scripts/ExampleDungeonUser.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleDungeonUser : MonoBehaviour 
+{
+	
+	public Transform playerPrefab;
+	public Transform floorPrefab;
+	public Transform enemyPrefab;
+	private int tileSize = 1;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		float seed = Random.value;
+		DungeonGenerator dg = new DungeonGenerator();
+
+		ArrayList arr = dg.generateDungeon(seed);
+		for (int i = 0; i < arr.Count; ++i)
+		{
+			Tile ts = (Tile)arr[i];
+			if (ts.type == "spawn")
+			{
+				Instantiate(playerPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.Euler( 0, 0, 0));
+			}
+			else if (ts.type == "enemy")
+			{
+				Instantiate(enemyPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.Euler( 0, 0, 0));
+			}
+			else if (ts.type == "floor")
+			{
+				Instantiate(floorPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.Euler( 0, 0, 0));
+			}
+		}
+	}
+}
