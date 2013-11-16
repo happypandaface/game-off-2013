f389bd4c18b1ade003a83b4b3ad99bfce943d98f
Gabriel Violette <phoniks7@hotmail.com>
2013-11-16 15:58:48 -0500

    Basically a revert. Bug fixed the tile generation. *Remember to pull before you push. -Deleted the DunGen project again. (It's just extra data right now?)

--- Changu/Assets/_Scripts/ExampleDungeonUser.cs
+++ Changu/Assets/_Scripts/ExampleDungeonUser.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ExampleDungeonUser : MonoBehaviour {
+public class ExampleDungeonUser : MonoBehaviour 
+{
 	
 	public Transform playerPrefab;
 	public Transform floorPrefab;
 	public Transform enemyPrefab;
 	public Transform wallPrefab;
-	private int tileSize = 10;
-	private float speed = 100;
+	private int tileSize = 1;
+	//private float speed = 100;
 	
-	void Start () {
+	void Start () 
+	{
 		float seed = Random.value;
 		DungeonGenerator dg = new DungeonGenerator();
 		dg.config(seed, 30, 15, 5);// parameters (random seed, width, height, # of rooms)
@@ -20,24 +22,26 @@ public class ExampleDungeonUser : MonoBehaviour {
 			Tile ts = (Tile)arr[i];
 			if (ts.type == "spawn")
 			{
-				Instantiate(playerPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, -.3f), Quaternion.Euler(-90, 0, 0));
-				transform.position = new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, -10);
-			}else
-			if (ts.type == "enemy")
+				Instantiate(playerPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.identity );
+				//transform.position = new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, -10); //I don't think this does what you think?
+			}
+			else if (ts.type == "enemy")
 			{
-				Instantiate(enemyPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, -.3f), Quaternion.Euler(-90, 0, 0));
-			}else
-			if (ts.type == "floor")
+				Instantiate(enemyPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.identity );
+			}
+			else if (ts.type == "floor")
 			{
-				Instantiate(floorPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, -.2f), Quaternion.Euler(-90, 0, 0));
-			}else
-			if (ts.type == "wall")
+				Instantiate(floorPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.identity );
+			}
+			else if (ts.type == "wall")
 			{
-				Instantiate(wallPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, -.2f), Quaternion.Euler(-90, 0, 0));
+				Instantiate(wallPrefab, new Vector3(ts.xPos*tileSize, ts.yPos*tileSize, 0.0f), Quaternion.identity );
 			}
 		}
 	}
-	
+
+	//Why move the generator?
+	/*
 	void Update () {
 		if (Input.GetKey(KeyCode.W))
 		{
@@ -56,4 +60,5 @@ public class ExampleDungeonUser : MonoBehaviour {
 			transform.Translate(-speed*Time.deltaTime,0,0);
 		}
 	}
+	*/
 }
